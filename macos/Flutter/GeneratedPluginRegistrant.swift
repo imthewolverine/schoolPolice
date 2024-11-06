82110f34caa97b3072d60fedc14e119527269727
Batlkhagva <batlhagva15@gmail.com>
2024-11-06 14:51:29 +0800

    yu uurchilsnu medql bn

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import firebase_core
 import flutter_secure_storage_macos
 import location
 import path_provider_foundation
-import sqflite_darwin
+import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
